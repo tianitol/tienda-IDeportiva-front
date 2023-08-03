8cee6d63da65188d87593592d00cff16a18f6ace
Tolvett Sepulveda <m.tolvett.sepulveda@accenture.com>
2023-08-03 12:53:25 -0400

    mapeando cards con clothes y accessories

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,6 +12,19 @@ const {shoes, accessories, clothes} = useContext(StateContext)
     <>
     <div className='card-container'>
     {
+        clothes.map((item) => {
+          return (
+            <ProductCard
+              key={item._id}
+              name={item.name}
+              description={item.description}
+              image={item.image}
+              // path={`/details/${event._id}`} />
+              />
+          )
+        })
+      }
+      {
         shoes.map((shoe) => {
           return (
             <ProductCard
@@ -19,6 +32,19 @@ const {shoes, accessories, clothes} = useContext(StateContext)
               name={shoe.name}
               description={shoe.description}
               image={shoe.image}
+              // path={`/details/${event._id}`} />
+              />
+          )
+        })
+      }
+      {
+        accessories.map((accessory) => {
+          return (
+            <ProductCard
+              key={accessory._id}
+              name={accessory.name}
+              description={accessory.description}
+              image={accessory.image}
               // path={`/details/${event._id}`} />
               />
           )
