fc4bf37e46b8bac761499e5532115c311186c4a3
Nicole Henríquez <ale.nicolehs@gmail.com>
2023-08-04 17:20:40 -0400

    Contact vista ok, falta darle la ruta para mostrar

---
+++ src/components/TextAreasFloat/index.jsx
@@ -0,0 +1,16 @@
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
+const TextAreasFloat = ({ label, placeholderText, valor }) => {
+  return (
+    <FloatingLabel controlId="floatingTextarea2" label={label}>
+      <Form.Control
+        as="textarea"
+        placeholder={placeholderText}
+        style={{ height: "100px" }}
+        ref={valor}
+      />
+    </FloatingLabel>
+  );
+};
+
+export default TextAreasFloat;
